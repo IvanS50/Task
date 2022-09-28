fcb5fdc4144fa752cc3a471e26e144215a4fa64d
IvanS50 <99894322+IvanS50@users.noreply.github.com>
2022-09-28 17:59:38 +0300

    Add files via upload

---
+++ ConsoleApp30/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Program
+{
+    public static string LongestCommonPrefix(string[] strs)
+    {
+        int len = strs.Length;
+        if (len == 0) return "";
+
+        string prefix = strs[0];
+        for (int i = 1; i < len; i++)
+        {
+            while (strs[i].IndexOf(prefix) != 0)
+            {
+                prefix = prefix.Substring(0, prefix.Length - 1);
+                if (prefix == "") return "";
+            }
+        }
+
+        return prefix;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Format("Words = flower, flow, flight; Answer = {0}", LongestCommonPrefix(new string[] { "flower", "flow", "flight" }))); // "fl"
+        Console.WriteLine(string.Format("Words = dog, racecar, car; Answer = {0}", LongestCommonPrefix(new string[] { "dog", "racecar", "car" }))); // ""
+    }
+}
